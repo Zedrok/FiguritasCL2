581ba58ecee76765029819894e6fc51037055ed4
Jose Silva <jose.silva.a@mail.pucv.cl>
2020-11-29 18:38:39 -0300

    cambios en forms

--- src/app/modules/auth/login/login.component.ts
+++ src/app/modules/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormGroup,FormControl} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  loginForm = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl('')
+
+  })
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+  onLogin(){
+    console.log('Formularios',this.loginForm.value) ;
+    alert('')
   }
 
 }
--- src/app/modules/auth/register/register.component.ts
+++ src/app/modules/auth/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup,FormControl} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
+  registerForm = new FormGroup({
+    nombre : new FormControl(''),
+    apellido: new FormControl(''),
+    email: new FormControl (''),
+    telefono:new FormControl(''),
+    password:new FormControl(''),
+  })
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  onRegister(){
+    console.log('Formulario : ',this.registerForm.value) ;
+  }
 }
